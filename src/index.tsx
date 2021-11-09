9a6be50ae00caf6e49f4a8b83b361684e7eb6e8c
Naoki Kobayashi <kobayashi@wantedly.com>
2021-11-09 13:51:26 +0900

    Implement Vurtialization component with react-intersection-observer

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Freeze } from "react-freeze";
+import { IntersectionOptions, useInView } from "react-intersection-observer";
+
+export interface VurtializationProps {
+  className?: string;
+  children: React.ReactChild;
+  intersectionOptions: IntersectionOptions;
+}
+
+export const Vurtialization: React.VFC<VurtializationProps> = ({
+  className,
+  children,
+  intersectionOptions,
+}) => {
+  const { ref, inView } = useInView(intersectionOptions);
+
+  return (
+    <div ref={ref} className={className}>
+      <Freeze freeze={!inView}>{children}</Freeze>
+    </div>
+  );
+};
